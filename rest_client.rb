16569716dd971bf62084d89c05b430c677398050
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-20 22:36:25 -0700

    I3: Add http body request

--- rest_client.rb
+++ rest_client.rb
@@ -7,6 +7,7 @@ search_path = '/search?q='
 
 google_query = RestClient.get(google + search_path + input)
 
+puts "Your Google search response has body #{google_query.body}"
 puts "Your Google search response has code #{google_query.code}"
 puts "Your Google search response has headers #{google_query.headers}"
 puts "Your Google search response has cookies #{bing_query.cookies}"
