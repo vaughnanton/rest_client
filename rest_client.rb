4c83d2628e0000810abd583220fe9527c3380a8d
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-20 22:34:54 -0700

    I2: Test http request complete

--- rest_client.rb
+++ rest_client.rb
@@ -1,1 +1,12 @@
-#initialize rest_client.rb
+require 'rest-client'
+
+print "What would you like to search on Google?"
+input = gets.chomp
+google = 'https://www.google.com/'
+search_path = '/search?q='
+
+google_query = RestClient.get(google + search_path + input)
+
+puts "Your Google search response has code #{google_query.code}"
+puts "Your Google search response has headers #{google_query.headers}"
+puts "Your Google search response has cookies #{bing_query.cookies}"
